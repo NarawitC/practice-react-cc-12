025b45f1033c39f9cf34f26ec5a3bad8dc7e820b
NarawitC <narawit.developer@outlook.com>
2022-08-19 01:03:57 +0700

    chore(categories): icon className

--- src/assets/categories.js
+++ src/assets/categories.js
@@ -1,23 +1,23 @@
 const categories = {
   webDevelopment: {
     text: 'Web Development',
-    icon: <i class='fa-solid fa-code'></i>,
+    icon: <i className='fa-solid fa-code'></i>,
   },
   entertainment: {
     text: 'Entertainment',
-    icon: <i class='fa-solid fa-film'></i>,
+    icon: <i className='fa-solid fa-film'></i>,
   },
   travel: {
     text: 'Travel',
-    icon: <i class='fa-solid fa-plane'></i>,
+    icon: <i className='fa-solid fa-plane'></i>,
   },
   shopping: {
     text: 'Shopping',
-    icon: <i class='fa-solid fa-shopping-cart'></i>,
+    icon: <i className='fa-solid fa-shopping-cart'></i>,
   },
   other: {
     text: 'Other',
-    icon: <i class='fa-solid fa-question-circle'></i>,
+    icon: <i className='fa-solid fa-question-circle'></i>,
   },
 };
 
